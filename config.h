5bf7839226c092c615f4a7cde1e309daf1006a23
Gabriel Pariat <gabrielpariat@gmail.com>
2021-01-13 02:09:41 -0500

    Test2

--- config.h
+++ config.h
@@ -1,4 +1,3 @@
-// Empty
 /* modifier 0 means no modifier */
 static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
 static char *fulluseragent  = ""; /* Or override the whole user agent string */
